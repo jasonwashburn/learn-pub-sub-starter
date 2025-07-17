f93473e1a856c1d3c9bfce4034400f4221cb2017
Jason Washburn <jason.washburn@gmail.com>
2025-07-17 07:03:56 -0500

    feat: add peril_topic queue creation on server startup

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -29,6 +29,12 @@ func main() {
 		os.Exit(1)
 	}
 
+	_, _, err = pubsub.DeclareAndBind(conn, routing.ExchangePerilDirect, routing.GameLogSlug, "game_logs.*", pubsub.Durable)
+	if err != nil {
+		fmt.Printf("Failed to declare and bind game logs queue: %s\n", err)
+		os.Exit(1)
+	}
+
 	gamelogic.PrintServerHelp()
 outerloop:
 	for {
